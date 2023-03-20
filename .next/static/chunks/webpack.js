582608378da8ae03086ed20a7501fca9e89f1d67
= <bbrowne14@my.bcit.ca>
2023-03-20 16:19:39 -0700

    backgrounds and animations

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "27e2ef4af7f4aaae"; }
+/******/ 		__webpack_require__.h = function() { return "5a7a88ee82f7afcc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
